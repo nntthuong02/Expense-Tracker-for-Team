993dce80a72d22e157f3d5df5e2ce1b164555516
nntthuong <thuong.nn204855@gmail.com>
2024-06-25 10:13:18 +0700

    ParticipantItem

---
+++ app/src/main/java/com/example/expensetrackerforteam/presentation/participant_screen/ParticipantDetailScreen.kt
@@ -0,0 +1,8 @@
+package com.example.expensetrackerforteam.presentation.participant_screen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ParticipantDetailScreen() {
+    
+}
---
+++ app/src/main/java/com/example/expensetrackerforteam/presentation/participant_screen/ParticipantViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.expensetrackerforteam.presentation.participant_screen
+
+class ParticipantViewModel {
+}
